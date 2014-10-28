3d10334bab221c7ed55a978bab232a5b5f2a5f11
Антон Миценгендлер <anton_mits@mail.ru>
2014-10-29 00:53:47 +0300

    Добавил SimpleStockFactory
    фабрика для парсера параметров складов

---
+++ ReDistr/IStockFactory.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ReDistr
+{
+    interface IStockFactory
+    {
+        IStockFactory CurrentFactory { get; } //возвращает ссылку на экземпляр фабрики
+        Stock GetStock(string stockSignature);     //возвращает экземпляр Stock или null
+                                              //stockName дескриптор склада
+        bool SetStockParams(string stockName, int minimum, int maximum, string signature); //устанавливает параметры для создаваемых экземляров
+                                                                                           //возвращает false если такая сигнатура уже существует
+    }
+}
