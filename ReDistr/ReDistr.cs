6a12d20e5df99845a9eba1ffb562d8c12413fdf1
Andrew <xigrus@yandex.ru>
2014-11-10 22:43:32 +0300

    Добавил заголовки

--- ReDistr/ReDistr.cs
+++ ReDistr/ReDistr.cs
@@ -11,12 +11,19 @@ namespace ReDistr
 		private const int RowFirstFillNumber = 2;
 		public static Control Control { get; set; }
 
+	
 		// Метод заполняет лист Test данными из словоря запчастей
 		public static void FillTestList(Dictionary<string, Item> items)
 		{
+			Control.Application.Worksheets[3].Cells.Clear();
 			// Заполняем заголовки
-			// TODO Дописать названия переменных
-			//Control.Application.Worksheets[3].Range["A1"].Value = items.First().Value.Id1C;
+			Control.Application.Worksheets[3].Range["A1"].Value = "Id1C";
+			Control.Application.Worksheets[3].Range["B1"].Value = "Name";
+			Control.Application.Worksheets[3].Range["C1"].Value = "Article";
+			Control.Application.Worksheets[3].Range["D1"].Value = "Manufacturer";
+			Control.Application.Worksheets[3].Range["E1"].Value = "StorageCategory";
+			Control.Application.Worksheets[3].Range["F1"].Value = "inBundle";
+			Control.Application.Worksheets[3].Range["G1"].Value = "inKit";
 			
 			var curentRow = RowFirstFillNumber;
 			foreach (KeyValuePair<string, Item> item in items)
